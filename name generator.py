a40f35d45f5700a2babf7cbca00f3e59c90f7cd2
Sam Machuga <machoog546@gmail.com>
2020-08-03 20:20:16 -0500

    adding use of notes

--- name generator.py
+++ name generator.py
@@ -7,7 +7,7 @@ personality = ["open", "driven", 'extravert ',
                  'guilt', 'depressed', 'lonely']
 trustLevel = ["parinoid", "trusting", 'neutral', 'flip-floppy']
 
-notes =  open('Char Notes.txt', 'w+')
+notes =  open('Char Notes.txt', 'a+')
 
 
 
@@ -17,16 +17,24 @@ while True:
     trust = random.choice(trustLevel)
     des1, des2 = random.choice(personality), random.choice(personality)
 
-
+    print()
     print("Name: " + person)
     print("Age: " + age)
     print("Trust level: " + trust)
     print("Personality traits: " + des1 + ", " + des2 )
+    print()
 
-    addNotes = input("Would you like to add notes?\ny to add, or enter to skip, e to exit: \n")
+    addNotes = input("Would you like to add notes?\n\ny to add, or enter to skip, e to exit: \n")
     if addNotes.lower() == 'y':
+        notes.write("Name: " + person + '\r')
+        notes.write("\tAge: " + str(age) + '\r')
+        notes.write("Trust level: " + trust + '\r')
+        notes.write("\tPersonality traits: " + des1 + ", " + des2  + '\r')
+        
         notesToAdd = input("Enter input below. Press return to save:\n")
-        notes.write(notesToAdd)
+        notes.write('\t--' + notesToAdd)
+        notes.write('\n\n')
+
     if addNotes.lower() == 'e':
         break
 
